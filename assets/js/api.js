f8dfd3eef878b13ecc923670307b5dfb3c80949c
huseyin <huseyingobekli29@gmail.com>
2023-06-10 23:26:48 +0300

    fetch url configuration completed

---
+++ assets/js/api.js
@@ -0,0 +1,26 @@
+const API_KEY = "209b666ef4d091a57b28950d2e0515f5";
+
+export const fetchDATA = function (URL, callback) {
+  fetch(`${URL}&appid=${API_KEY}`)
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
+
+export const url = {
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`;
+  },
+
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`;
+  },
+  airPollution(lat, lon) {
+    return `http://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
+  geo(quey) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${quey}&limit=5`;
+  },
+};
