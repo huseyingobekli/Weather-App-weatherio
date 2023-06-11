67c49fac756786b2a76134d0f85828b9aa5bb2ac
huseyin <huseyingobekli29@gmail.com>
2023-06-11 23:48:10 +0300

    route configurations completed

--- assets/js/module.js
+++ assets/js/module.js
@@ -62,5 +62,29 @@ export const mps_tp_kmh = (mps) => {
 // will be created
 
 export const aqiText = {
-  1: {},
+  1: {
+    level: "Good",
+    message:
+      "Air quality is considered satisfactory, and air pollution poses little or no risk",
+  },
+  2: {
+    level: "Fair",
+    message:
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution.",
+  },
+  3: {
+    level: "Moderate",
+    message:
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+  },
+  4: {
+    level: "Poor",
+    message:
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  5: {
+    level: "Very Poor",
+    message:
+      "Air quality is acceptable; however, for some pollutants there may be a moderate health concern for a very small number of people who are unusually sensitive to air pollution. ",
+  },
 };
