867def8e60609192dbb33752200dfa0781503a6f
huseyin <huseyingobekli29@gmail.com>
2023-06-14 23:56:47 +0300

    current-weather API integration completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -60,3 +60,90 @@ searchFiled,
       }, searchTimeOutDuration);
     }
   });
+
+const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector(
+  "[data-current-location-btn]"
+);
+const errorContent = document.querySelector("[data-error-content]");
+
+export const updateWeather = function (lat, lon) {
+  loading.style.display = "grid";
+  container.style.overflowY = "hidden";
+
+  container.classList.contains("fade-in") ??
+    container.classList.remove("fade-in");
+  errorContent.style.display = "none";
+
+  const currentWeatherSection = document.querySelector(
+    "[data-current-weather]"
+  );
+  const highlightSection = document.querySelector("[data-highlights]");
+  const hourlySection = document.querySelector("[data-hourly-forecast]");
+  const forecastSection = document.querySelector("[data-5-dat-forecast]");
+
+  currentWeatherSection.innerHTML = "";
+  highlightSection.innerHTML = "";
+  hourlySection.innerHTML = "";
+  forecastSection.innerHTML = "";
+
+  if (window.location.hash === "#/current-location") {
+    currentLocationBtn.setAttribute("disabled", "");
+  } else {
+    currentLocationBtn.removeAttribute("disabled");
+  }
+
+  fetchDATA(url.currentWeather(lat, lon), function (currentWeather) {
+    const {
+      weather,
+      dt: dateUnix,
+      sys: { sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC },
+      main: { temp, feels_like, pressure, humidity },
+      visiblity,
+      timezone,
+    } = currentWeather;
+
+    const [{ description, icon }] = weather;
+    const card = document.createElement("div");
+    card.classList.add("card", "card-lg", "current-weather-card");
+
+    card.innerHTML = `
+    
+        <h2 class="title-2 card-title">Now</h2>
+        <div class="wrapper">
+          <p class="heading">${parseInt(temp)}&deg;c</p>
+          <img
+            src="./assets/images/weather_icons/${icon}.png"
+            width="64"
+            height="64"
+            alt="${description}"
+            class="weather-icon"
+          />
+        </div>
+
+        <p class="body-3">${description}</p>
+        <ul class="meta-list">
+          <li class="meta-item">
+            <span class="m-icon">calendar_today</span>
+            <p class="title-3 meta-text">${module.getDate(
+              dateUnix,
+              timezone
+            )}</p>
+          </li>
+
+          <li class="meta-item">
+            <span class="m-icon">location_on</span>
+            <p class="title-3 meta-text" data-location></p>
+          </li>
+        </ul>
+        
+    `;
+
+    fetchDATA(url.reverseGeo(lat, lon), function ([{ name, country }]) {
+      card.querySelector("[data-location]").innerHTML = `${name}, ${country}`;
+    });
+
+    currentWeatherSection.appendChild(card);
+  });
+};
